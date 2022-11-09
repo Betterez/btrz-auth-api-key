a19ef404dfb21c3d92dd38af4961b97e4a098531
Fblind <fblind@gmail.com>
2022-11-09 13:00:26 -0300

    #3rr0ec6: Add API_CLIENT audience

--- audiences.js
+++ audiences.js
@@ -1,5 +1,6 @@
 module.exports = {
   CUSTOMER: "customer",
   BETTEREZ_APP: "betterez-app",
-  MOBILE_SCANNER: "btrz-mobile-scanner"
+  MOBILE_SCANNER: "btrz-mobile-scanner",
+  API_CLIENT: "btrz-api-client"
 };
